414d71a4e6eb6cde184dbcb9012d4a40c40fdd18
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-05-13 14:50:16 +0200

    Fix typo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- get_token.sh
+++ get_token.sh
@@ -3,4 +3,4 @@ if ( [ -z "$AWS_PROFILE" ] || [ -z "$KUBECONFIG" ] )
 then
   echo "$0: you should specify AWS_PROFILE=... and KUBECONFIG=..."
 fi
-kubectl -n kube-system describe secret $(cncfkubectl.sh -n kube-system get secret | grep admin-user | awk '{print $1}')
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
